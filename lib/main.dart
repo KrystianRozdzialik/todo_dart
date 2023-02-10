13345654cd23f3879e039c9eee433224d09ee95c
Krystian <kryroz21@gmail.com>
2023-02-10 10:53:06 +0100

    app without styles

--- lib/main.dart
+++ lib/main.dart
@@ -21,18 +21,19 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   //List todo = List();
 
+  //final List<String> items = List<String>.generate(10000, (i) => '$i');
   String input = "";
 
-  var todo = [""];
+  var todo = ["Uśmiechnij się!"];
 
-  @override
-  void initState() {
-    super.initState();
-    todo.add("item1");
-    todo.add("item2");
-    todo.add("item3");
-    todo.add("item4");
-  }
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   todo.add("item1");
+  //   todo.add("item2");
+  //   todo.add("item3");
+  //   todo.add("item4");
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -45,12 +46,24 @@ class _MyAppState extends State<MyApp> {
               context: context,
               builder: (BuildContext context){
                 return AlertDialog(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8)),
                   title: Text("Add ToDo"),
                   content: TextField(
-                    onChanged: (String value ){
+                    onChanged: (String value) {
                       input = value;
                     },
                   ),
+                  actions: <Widget>[
+                    TextButton(
+                      onPressed: () {
+                        setState(() {
+                          todo.add(input);
+                        });
+                        Navigator.of(context).pop();
+                      },
+                      child: Text("Add"))
+                  ],
               );
             });
         },
@@ -60,12 +73,27 @@ class _MyAppState extends State<MyApp> {
         ),
       ),
       body: ListView.builder(
-          itemCount:todo.length,
-          itemBuilder: (BuildContext context, int index){
-            return Dismissible(key: Key(todo[index]),
+          itemCount: todo.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Dismissible(
+                key: Key(todo[index]),
                 child: Card(
+                  elevation: 2,
+                  margin: EdgeInsets.all(8),
+                  shape: RoundedRectangleBorder(borderRadius:
+                  BorderRadius.circular(8)),
                   child: ListTile(
                     title: Text(todo[index]),
+                    trailing: IconButton(
+                      icon: Icon(
+                          Icons.delete,
+                          color: Colors.red
+                      ),
+                      onPressed: (){
+                        setState(() {
+                          todo.removeAt(index);
+                        });
+                      }),
                   ),
                 ));
           }),
